48d95c0081140016dbc2a0c350ddc05ba1cf0f8e
Ana Beatriz Costa de Sousa <anacostasousa08@gmail.com>
2022-03-04 22:32:33 -0300

    finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine(@"I woke up in a dream today
+To the cold of the static and put my cold feet on the floor
+Forgot all about yesterday
+Remembering I'm pretending to be where I'm not anymore
+A little taste of hypocrisy
+And I'm left in the wake of the mistake, slow to react
+Even though you're so close to me
+You're still so distant and I can't bring you back");
+
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"It's true
+The way I feel
+Was promised by your face
+The sound of your voice
+Painted on my memories
+Even if you're not with me");
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"I'm with you
+You now I see
+Keeping everything inside
+You now I see
+Even when I close my eyes");
+
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(@"I hit you and you hit me back
+We fall to the floor, the rest of the day stands still
+Fine line between this and that
+When things go wrong I pretend that the past isn't real
+Now I'm trapped in this memory
+And I'm left in the wake of the mistake, slow to react
+So even though you're close to me
+You're still so distant and I can't bring you back");
+
+Console.ResetColor();
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine(@"It's true
+The way I feel
+Was promised by your face
+The sound of your voice
+Painted on my memories
+Even if you're not with me");
+Console.ResetColor();
